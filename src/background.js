940f90e7f44f4eefcaf4ab1126c00f4715eeffad
Michael Zink <20622728+laser-brain@users.noreply.github.com>
2021-03-28 23:22:06 +0200

    reincluded resizable: false

--- src/background.js
+++ src/background.js
@@ -30,7 +30,7 @@ async function createWindow() {
   // Create the browser window.
   win = new BrowserWindow({
     frame: false,
-    // resizable: false,
+    resizable: false,
     alwaysOnTop: true,
     width: 200,
     height: 223,
